1ea8ea74269af62d3c9891eba5f2819350a22749
Low-go <lorranalvesgaldino98@gmail.com>
2024-11-17 23:27:34 -1000

    add character controller for movment. modify gravity and jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,29 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
-    private Rigidbody theRB;
     public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
 
     // Start is called before the first frame update
     void Start()
     {
-        theRB = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
 
         if (Input.GetButtonDown("Jump"))
         {
-            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+            moveDirection.y = jumpForce;
         }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale); // this is to add gravity after a jump
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }

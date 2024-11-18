1200cef594f4beeaa136669dfacd5f642095d4de
Low-go <lorranalvesgaldino98@gmail.com>
2024-11-18 09:19:46 -1000

    camera follows player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public Vector3 offSet;
+    public bool useOffsetValues;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!useOffsetValues)
+        {
+            offSet = target.position - transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.LookAt(target);
+        transform.position = target.position - offSet;
     }
 }

881fa803fec4a1df1ef3cf9522edda7ce195afe6
Low-go <lorranalvesgaldino98@gmail.com>
2024-11-24 17:02:58 -1000

    add invincibility and respawn

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -8,25 +8,92 @@ public class HealthManager : MonoBehaviour
     public int currentHealth;
 
     public PlayerController thePlayer;
+    public float invincibilityLength;
+    private float invincibilityCounter;
+
+    public Renderer playerRenderer;
+    public float flashCounter;
+    public float flashLength = 0.1f;
+
+    //respawn values, take not for future
+    private bool isRespawning;
+    private Vector3 respawnPoint;
    
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
-        thePlayer = FindObjectOfType<PlayerController>();
+        //thePlayer = FindObjectOfType<PlayerController>();
+        
+        respawnPoint = thePlayer.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // time period player does not take damage
+        if (invincibilityCounter > 0)
+        {
+            invincibilityCounter -= Time.deltaTime;
+
+            flashCounter -= Time.deltaTime;
+            if(flashCounter <= 0)
+            {
+                playerRenderer.enabled = !playerRenderer.enabled;
+                flashCounter = flashLength;
+            }
+
+            // make sure its on when damage is done
+            if(invincibilityCounter <= 0)
+            {
+                playerRenderer.enabled = true;
+            }
+        }
     }
+
 
     public void hurtPlayer(int damage, Vector3 direction)
     {
-        currentHealth -= damage;
+        // only take damage if player is not within invincibility time
+        if (invincibilityCounter <= 0)
+        {
+            currentHealth -= damage;
 
-        thePlayer.knockBack(direction);
+            // do i really want this? respawn
+            if(currentHealth <= 0)
+            {
+                Respawn();
+            }
+            else
+            {
+                thePlayer.knockBack(direction);
+
+                invincibilityCounter = invincibilityLength;
+
+                playerRenderer.enabled = false;
+                flashCounter = flashLength;
+            }
+
+            //thePlayer.knockBack(direction);
+
+            //invincibilityCounter = invincibilityLength;
+
+            //playerRenderer.enabled = false;
+            //flashCounter = flashLength;
+        }
+    }
+
+    public void Respawn()
+    {
+        CharacterController charController = thePlayer.GetComponent<CharacterController>();
+
+        // Disable controller, move player, then re-enable
+        charController.enabled = false;
+        thePlayer.transform.position = respawnPoint;
+        charController.enabled = true;
+
+        // Reset health
+        currentHealth = maxHealth;
     }
 
     public void healPlayer(int healAmount)
